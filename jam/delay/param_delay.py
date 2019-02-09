91116221f08e8bf390198732b274f2b43c3cf9f1
OcHe <soundbooze@gmail.com>
2019-02-09 18:58:00 +0700

    pre

---
+++ jam/delay/param_delay.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import sys
+import socket, time
+import subprocess as sp
+
+# get mod-host pid
+pid = sp.check_output("pgrep mod-host; exit 0", shell=True)
+if pid == '':
+    print 'mod-host is not running'
+    exit(0)
+
+# setup socket
+s = socket.socket()
+s.connect(('127.0.0.1', 5555))
+s.settimeout(5)
+
+def check_mod_host():
+    if sp.check_output("pgrep mod-host; exit 0", shell=True) != pid:
+        print 'mod-host died'
+        exit(1)
+
+def send_command(command):
+    s.send(command)
+    print 'sent:', command
+    check_mod_host()
+
+    try:
+        resp = s.recv(1024)
+        if resp: print 'resp:', resp
+        return True
+
+    except Exception:
+        return False
+
+#send_command('add http://calf.sourceforge.net/plugins/VintageDelay 0')
+
+bpm = float(sys.argv[1]) / 2.0
+
+send_command('param_set 0 bpm_host %f' % bpm)
+send_command('param_set 0 bpm %f' % bpm)
+send_command('param_set 0 mix_mode 2')
+send_command('param_set 0 medium 0')
+send_command('param_set 0 time_l 2')
+send_command('param_set 0 time_r 2')
+
+#send_command('remove 0')
---
+++ jam/run.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+jalv.gtk3 http://gareus.org/oss/lv2/convoLV2#Stereo &
+guitarix &
+jack_thru &
+
+sleep 3
+
+jack_disconnect 'jack_thru:output_1' 'system:playback_1'
+jack_disconnect 'jack_thru:output_2' 'system:playback_2'
+
+jack_connect 'system:capture_1' 'gx_head_amp:in_0'
+
+jack_connect 'gx_head_fx:out_0' 'LV2 Convolution Stereo:in_1'
+jack_connect 'gx_head_fx:out_1' 'LV2 Convolution Stereo:in_2'
+
+jack_connect 'LV2 Convolution Stereo:out_1' 'system:playback_1'
+jack_connect 'LV2 Convolution Stereo:out_2' 'system:playback_2'
+
+jack_connect 'LV2 Convolution Stereo:out_1' 'effect_0:in_l' 
+jack_connect 'LV2 Convolution Stereo:out_2' 'effect_0:in_r' 
+
+jack_connect 'effect_0:out_l' 'system:playback_1'
+jack_connect 'effect_0:out_r' 'system:playback_2'
+
+# Jack Thru
+
+jack_connect 'LV2 Convolution Stereo:out_1' 'jack_thru:input_1'
+jack_connect 'LV2 Convolution Stereo:out_2' 'jack_thru:input_2'
+
+jack_connect 'effect_0:out_l' 'jack_thru:input_1'
+jack_connect 'effect_0:out_r' 'jack_thru:input_2'
