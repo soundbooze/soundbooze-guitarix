cd1e461936b60da0546342411ecef12fdae100c0
bruno/br <soundbooze@gmail.com>
2019-02-10 18:11:16 +0700

    Add files via upload

--- jam/delay/run.sh
+++ jam/delay/run.sh
@@ -6,9 +6,9 @@ python init.py > /dev/null 2>&1
 
 while [ 1 ]
 do
-  jack_capture_ms -d 500 -p system:playback* -f wav out.wav > /dev/null 2>&1
+  jack_capture_ms -d 1000 -p system:playback* -f wav out.wav > /dev/null 2>&1
   ffmpeg -i out.wav tmp.wav > /dev/null 2>&1
   BPM=`aubio tempo tmp.wav | awk '{ print $1 }'`
-  python param_delay.py $BPM
+  python param_delay.py $BPM > /dev/null 2>&1
   rm -f out.wav tmp.wav
 done
